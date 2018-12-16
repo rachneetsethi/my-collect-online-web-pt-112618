f37949537f9914a1a0521a75c9b66df8224eec75
Rachneet <rachneetsethi@gmail.com>
2018-12-16 03:50:54 +0000

    Automatically backed up by Learn

--- spec/my_collect_spec.rb
+++ spec/my_collect_spec.rb
@@ -50,14 +50,6 @@ describe "my_collect" do
   end
 end
 
-  def my_collect (array)
-  i = 0 
-  collection = [ ]
-  while i < array.length
-  collection << yield (array[i])
-  i += 1
-  end
-collection 
-end
+
 
 
