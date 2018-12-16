5c170b7be4d16dd942afe3413f3104b825fea921
Rachneet <rachneetsethi@gmail.com>
2018-12-16 03:55:52 +0000

    Automatically backed up by Learn

--- spec/my_collect_spec.rb
+++ spec/my_collect_spec.rb
@@ -55,5 +55,16 @@ my_collect(array) do |name|
   name.split(" ").first
 end
 
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield (array[i])
+    i += 1
+  end
+  collection
+end
+
+my_collect ()
 
 
